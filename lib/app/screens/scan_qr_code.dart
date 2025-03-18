9c09b6885f0ff3db4875c6bc05d341e620a1b9a8
Salah Fathy <SalahFathy@gmail.com>
2025-03-18 21:46:01 +0200

    first commit

--- lib/app/screens/scan_qr_code.dart
+++ lib/app/screens/scan_qr_code.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:qr_code_scanner_plus/qr_code_scanner_plus.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ScanQrCode extends StatefulWidget {
   const ScanQrCode({super.key});
@@ -15,71 +17,144 @@ class _ScanQrCodeState extends State<ScanQrCode> {
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
   Barcode? result;
   QRViewController? controller;
-
-  @override
-  void initState() {
-    super.initState();
-  }
+  bool isDialogOpen = false;
 
   @override
   void reassemble() {
     super.reassemble();
-    if (Platform.isAndroid) {
-      controller!.pauseCamera();
-    } else if (Platform.isIOS) {
-      controller!.resumeCamera();
+    if (controller != null) {
+      if (Platform.isAndroid) {
+        controller!.pauseCamera();
+      } else if (Platform.isIOS) {
+        controller!.resumeCamera();
+      }
     }
   }
 
   void _onQRViewCreated(QRViewController controller) {
     this.controller = controller;
     controller.scannedDataStream.listen((scanData) {
-      setState(() {
-        result = scanData;
-      });
+      if (!isDialogOpen && (result == null || result!.code != scanData.code)) {
+        setState(() {
+          result = scanData;
+        });
+        _showQRDialog(scanData.code!);
+      }
     });
+  }
+
+  Future<void> _showQRDialog(String qrCode) async {
+    setState(() => isDialogOpen = true);
+    Uri qrLink = Uri.parse(qrCode);
+
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return AlertDialog(
+          title: Column(
+            children: [
+              Text(
+                "QR Code Scanned",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    onTap: () async {
+                      if (await canLaunchUrl(qrLink)) {
+                        await launchUrl(qrLink);
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text("Could not open link")),
+                        );
+                      }
+                    },
+                    child: Text(
+                      qrCode,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.blue,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.copy, color: Colors.grey),
+                    onPressed: () {
+                      Clipboard.setData(ClipboardData(text: qrCode));
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text("Copied to clipboard")),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+          content: SizedBox(
+            width: MediaQuery.of(context).size.width * 0.8,
+            height: MediaQuery.of(context).size.width * 0.8,
+            child: Center(
+              child: QrImageView(
+                foregroundColor: Colors.black,
+                eyeStyle: const QrEyeStyle(eyeShape: QrEyeShape.circle),
+                data: qrCode,
+                version: QrVersions.auto,
+                size: MediaQuery.of(context).size.width * 0.6,
+              ),
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+                setState(() => isDialogOpen = false);
+                controller?.resumeCamera();
+              },
+              child: Text("OK"),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Scan QR Code'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(height: 20),
-            Expanded(
-              flex: 5,
-              child: QRView(key: qrKey, onQRViewCreated: _onQRViewCreated),
-            ),
-            SizedBox(height: 40),
-            Expanded(
-              flex: 1,
-              child: Center(
-                child:
-                    (result != null)
-                        ? Text(
-                          'Data: ${result!.code}',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        )
-                        : Text(
-                          'QR Content Here',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
+      appBar: AppBar(title: Text('Scan QR Code'), centerTitle: true),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 5,
+            child: QRView(key: qrKey, onQRViewCreated: _onQRViewCreated),
+          ),
+          Expanded(
+            flex: 1,
+            child: Center(
+              child:
+                  (result != null)
+                      ? Text(
+                        'Data: ${result!.code}',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
                         ),
-              ),
+                      )
+                      : Text(
+                        'QR Content Here',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
